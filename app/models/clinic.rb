59a63c8acd19781b636426ea68e45581483964d0
Yun Jia <yunjia@me.com>
2014-02-28 20:46:50 -0800

    Added basic attributes.

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,6 +1,12 @@
 class Clinic < ActiveRecord::Base
+  # Set the table name
   self.table_name = "dummy_hmis"
-  alias_attribute :name, :ProviderName
+
+  # Set the column name for each attribute
+  alias_attribute :name,    :ProviderName
+  alias_attribute :address, :AddressLine1
+  alias_attribute :city,    :AddressCity
+  alias_attribute :zip,     :AddressPostalCode
 
   # Apply a default code to the query.
   default_scope { where("ServiceCode = 'CLINIC'") }
