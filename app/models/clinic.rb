91b2e69bcdb0bf72b75b50ebae7df961d250dae3
Yun Jia <yunjia@me.com>
2014-02-28 20:30:12 -0800

    Added mysql connection.

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,2 +1,8 @@
 class Clinic < ActiveRecord::Base
+  self.table_name = "dummy_hmis"
+  alias_attribute :name, :ProviderName
+
+  # Apply a default code to the query.
+  default_scope { where("ServiceCode = 'CLINIC'") }
 end
+
